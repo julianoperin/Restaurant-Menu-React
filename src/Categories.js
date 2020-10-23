6d7693350a25c63938edb105e254e593a00ec177
‘Juliano <julianoperins@gmail.com>
2020-10-23 13:42:54 -0400

    refactored the btns categories

--- src/Categories.js
+++ src/Categories.js
@@ -1,20 +1,26 @@
 import React from "react";
 
-const Categories = ({ filterItems }) => {
+const Categories = ({ categories, filterItems }) => {
   return (
     <div className="btn-container">
-      <button className="filter-btn" onClick={() => filterItems("")}>
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            className="filter-btn"
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+      {/* <button className="filter-btn" onClick={() => filterItems("all")}>
         all
       </button>
       <button className="filter-btn" onClick={() => filterItems("breakfast")}>
         breakfast
-      </button>
-      <button className="filter-btn" onClick={() => filterItems("lunch")}>
-        lunch
-      </button>
-      <button className="filter-btn" onClick={() => filterItems("shakes")}>
-        shakes
-      </button>
+      </button> */}
     </div>
   );
 };
